e7be84babfcb1c98a647a9e2f3110c37ec2f72f9
ThiliniF <ict10009@gmail.com>
2026-03-15 17:52:42 +0530

    Removing Login.feature.cs auto generated file from tracking and adding a wait to the inventory page.

--- src/SwagLabs.PageObjects/Pages/InventoryItemPage.cs
+++ src/SwagLabs.PageObjects/Pages/InventoryItemPage.cs
@@ -10,10 +10,12 @@ namespace SwagLabs.PageObjects.Pages
     public class InventoryItemPage : BasePage
     {
         private readonly By addToCartBtn = ByTestId("add-to-cart");
+        private readonly By itemName = ByTestId("inventory-item-name");
         public InventoryItemPage(IWebDriver driver, TestConfiguration config) : base(driver, config) { }
 
         public void AddToCart()
         {
+            WaitForElement(itemName);
             WaitForClickable(addToCartBtn).Click();
         }
 
