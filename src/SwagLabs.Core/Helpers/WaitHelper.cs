9d554f7de27be088002e3816cdde6a54ae7ee835
ThiliniF <ict10009@gmail.com>
2026-03-15 18:04:33 +0530

    Increasing timeouts for capture elements.

--- src/SwagLabs.Core/Helpers/WaitHelper.cs
+++ src/SwagLabs.Core/Helpers/WaitHelper.cs
@@ -6,19 +6,19 @@ namespace SwagLabs.Core.Helpers
 {
     public static class WaitHelper
     {
-        public static IWebElement WaitForElement(IWebDriver driver, By locator, int seconds = 10)
+        public static IWebElement WaitForElement(IWebDriver driver, By locator, int seconds = 20)
         {
             var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
             return wait.Until(ExpectedConditions.ElementIsVisible(locator));
         }
 
-        public static IWebElement WaitForElementClickable(IWebDriver driver, By locator, int seconds = 10)
+        public static IWebElement WaitForElementClickable(IWebDriver driver, By locator, int seconds = 20)
         {
             var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
             return wait.Until(ExpectedConditions.ElementToBeClickable(locator));
         }
 
-        public static bool WaitForUrl(IWebDriver driver, string partialUrl, int seconds = 10)
+        public static bool WaitForUrl(IWebDriver driver, string partialUrl, int seconds = 20)
         {
             var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
             return wait.Until(ExpectedConditions.UrlContains(partialUrl));
